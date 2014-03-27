03f2db0d85ae1243147c1cf6ff93530a72c4bfef
Kaifin <cmentzer85@gmail.com>
2014-03-27 16:12:50 -0400

    updated UI

--- Pharmacy Records/Pharmacy Records/Form1.Designer.cs
+++ Pharmacy Records/Pharmacy Records/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.logout = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // logout
+            // 
+            this.logout.Location = new System.Drawing.Point(339, 273);
+            this.logout.Name = "logout";
+            this.logout.Size = new System.Drawing.Size(75, 23);
+            this.logout.TabIndex = 0;
+            this.logout.Text = "log out";
+            this.logout.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(674, 319);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.logout);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Name = "Form1";
             this.Text = "Pharmacy Database";
             this.ResumeLayout(false);
@@ -42,6 +66,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button logout;
+        private System.Windows.Forms.Button button2;
     }
 }
 
